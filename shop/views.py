283c4d1b3ec966e8c23de60da2237b2114e58a60
CMAK12 <dsumcow@gmail.com>
2023-01-12 21:28:47 +0200

    Add Sort

--- shop/views.py
+++ shop/views.py
@@ -2,13 +2,24 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
 def home(request, category_slug=None):
+	sort_by = request.GET.get('sort')
 	category_page = None
 	products = None
 	if category_slug != None:
-		category_page = get_object_or_404(Category, slug=category_slug)
-		products = Product.objects.filter(category=category_page, available=True)
+			category_page = get_object_or_404(Category, slug=category_slug)
+			if sort_by == 'expensive':
+				products = Product.objects.filter(category=category_page, available=True).order_by('-price')
+			elif sort_by == 'cheap':
+				products = Product.objects.filter(category=category_page, available=True).order_by('price')
+			else:
+				products = Product.objects.filter(category=category_page, available=True).order_by('name')
 	else:
-		products = Product.objects.all().filter(available=True)
+		if sort_by == 'expensive':
+			products = Product.objects.all().filter(available=True).order_by('-price')
+		elif sort_by == 'cheap':
+			products = Product.objects.all().filter(available=True).order_by('price')
+		else:
+			products = Product.objects.all().filter(available=True).order_by('name')
 	return render(request, 'home.html', {'category':category_page, 'products': products})
 
 def product(request, category_slug, product_slug):
@@ -20,13 +31,21 @@ def product(request, category_slug, product_slug):
 
 def search(request):
 	search_query = request.GET['q']
+	sort_by = request.GET.get('sort')
 
 	if search_query == None:
-		searching = Product.objects.all().filter(available=True).order_by('price')
-		return render(request, 'search.html', {'searches':searching})
+		if sort_by == 'expensive':
+			searching = Product.objects.all().filter(available=True).order_by('price')
+		elif sort_by == 'cheap':
+			searching = Product.objects.all().filter(available=True).order_by('-price')
+		else:
+			searching = Product.objects.all().filter(available=True).order_by('name')
 	else:
-		searching = Product.objects.filter(
-            name__contains=search_query,
-			available=True
-        ).order_by('price')
-		return render(request, 'search.html', {'searches':searching})
+		if sort_by == 'expensive':
+			searching = Product.objects.filter(name__contains=search_query, available=True).order_by('price')
+		elif sort_by == 'cheap':
+			searching = Product.objects.filter(name__contains=search_query, available=True).order_by('-price')
+		else:
+			searching = Product.objects.filter(name__contains=search_query, available=True).order_by('name')
+	quantity_of_goods = len(searching)
+	return render(request, 'search.html', {'searches':searching, 'quantity':quantity_of_goods})
